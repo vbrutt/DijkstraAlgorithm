99eecd8db8739f31a8a3a852435477962412e241
Vera Brütt <Vera.Bruett@heuboe.de>
2018-08-20 11:02:02 +0200

    last changes

--- src/test/java/de/heuboe/ausbildung/DijkstraAlgorithm/OutputTest.java
+++ src/test/java/de/heuboe/ausbildung/DijkstraAlgorithm/OutputTest.java
@@ -2,7 +2,6 @@ package de.heuboe.ausbildung.DijkstraAlgorithm;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.*;
 import java.util.*;
